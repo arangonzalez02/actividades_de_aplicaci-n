bd68926de061bd5f5f6a7eef81d4c86899291da2
arangonzalez02 <aran.gonzalez.garcia02@gmail.com>
2024-05-24 15:07:40 +0200

    cambios

--- script/2_16.js
+++ script/2_16.js
@@ -1,2 +1,7 @@
 //Desarrolla un programa con la lógica que desees donde expliques las diferencias de usar los operadores a++, a--, ++a y --a.Utiliza los comentarios de una y varias líneas en tu explicación.
 
+let number;
+
+number = Number (prompt("Introduce un numero: "));
+
+
