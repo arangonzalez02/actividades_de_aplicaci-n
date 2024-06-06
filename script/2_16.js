c51ea6ea458d527c5d29abb4c2a115077181ae3c
arangonzalez02 <aran.gonzalez.garcia02@gmail.com>
2024-06-06 13:59:01 +0200

    cambios

--- script/2_16.js
+++ script/2_16.js
@@ -1,4 +1,4 @@
-//Desarrolla un programa con la lógica que desees donde expliques las diferencias de usar los operadores a++, a--, ++a y --a.Utiliza los comentarios de una y varias líneas en tu explicación.
+//Desarrolla un programa con la lógica que desees donde expliques las diferencias de usar los operadores a++, a--, ++a y --a. Utiliza los comentarios de una y varias líneas en tu explicación.
 
 let a = 10;
 
--- script/2_18.js
+++ script/2_18.js
@@ -1,12 +1,12 @@
 //Idea un programa en el que usar un bucle do while sea mejor idea que usar un bucle while.
 
 
-let usuNum;
+let usuNum = 0;
 
 do {
-    usuNum = prompt("Ingresa un número entre 1 y 10:");
-    usuNum = Number(usuNum);  // Convertimos la entrada a número
-} while (isNaN(usuNum) || usuNum < 1 || usuNum > 10);
+    usuNum = Number (prompt("Ingresa un número entre 1 y 10:"));
+    
+} while (usuNum < 1 || usuNum > 10);  // || ->  ESTO O ESTO
 
 console.log("Número válido ingresado:", usuNum);
 
